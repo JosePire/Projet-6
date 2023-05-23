545e1de2b92f00fe76796b32d9d912c9ffbdcda7
Jose <piresjose_92@hotmail.fr>
2023-05-23 11:28:57 +0200

    IW-26 Etape1.1 terminé

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -1,12 +1,29 @@
-console.log('salut')
+const divGallery = document.getElementById('gallery')
 
 const API_URL = 'http://localhost:5678/api'
-let projects
 
-fetch(`${API_URL}/works`, {
+const getWorks = async () => fetch(`${API_URL}/works`, { method: 'get' }).then(res => res.json())
 
-  method: 'get'
-}).then(res => {
-  projects = res.json()
+const init = async () => {
+  const projects = await getWorks()
   console.log(projects)
-})
+
+  // loop on each project
+  projects.forEach(item => {
+    // create figure and append to divGallery
+    const figure = document.createElement('figure')
+    divGallery.appendChild(figure)
+    // create img and append to figure
+    const img = document.createElement('img')
+    img.setAttribute('src', item.imageUrl)
+    img.setAttribute('alt', item.title)
+    img.setAttribute('crossorigin', 'anonymous')
+    figure.appendChild(img)
+    // create figCaption text and append to figure
+    const figCaption = document.createElement('figCaption')
+    figCaption.innerHTML = item.title
+    figure.appendChild(figCaption)
+  });
+}
+
+init()
